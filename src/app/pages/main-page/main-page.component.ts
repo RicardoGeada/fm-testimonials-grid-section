32af1cc6f29eca1b6bffc3bcae02f4ffa8a7a5b4
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-05-21 15:16:58 +0200

    feat(testimonial-card): create component with initial HTML layout

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TestimonialCardComponent } from "../../shared/components/testimonial-card/testimonial-card.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [TestimonialCardComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
